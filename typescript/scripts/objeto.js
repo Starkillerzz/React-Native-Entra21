6632c7d6ab65efea02f03b92f035ae599694bfb2
Ruan Karl Alves de Souza <91505856+Starkillerzz@users.noreply.github.com>
2022-06-15 20:37:17 -0300

    Delete typescript/scripts directory

--- typescript/scripts/objeto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var fornecedor = { id: 0, nome: "", email: "", fone: "", cpf: "" };
-fornecedor.id = 10;
-fornecedor.nome = "antartica";
-fornecedor.email = "pinguim@gmail.com";
-fornecedor.fone = "666-666";
-fornecedor.cpf = 895;
-console.log(fornecedor);
