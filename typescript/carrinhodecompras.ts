a71c18a9923d15ab29c82a880e45feeda7d70fd3
Ruan Karl Alves de Souza <ruansouza5@gmail.com>
2022-06-15 21:08:05 -0300

    feito carrinho de compras como objeto

---
+++ typescript/carrinhodecompras.ts
@@ -0,0 +1,15 @@
+interface Compras {
+    id:number
+    item: string
+    quantidade: number
+    preco: number
+}
+
+var carrinho: Compras ={id: 0, item: "", quantidade: 0, preco: 0}
+
+carrinho.id = 1
+carrinho.item = "Feijão"
+carrinho.quantidade = 2
+carrinho.preco = 7.5
+
+console.log(carrinho)
