251e91d92d117bdb339b1be207227143e6196f3b
Ruan Karl Alves de Souza <ruansouza5@gmail.com>
2022-06-22 20:38:13 -0300

    aplicado mais métodos no código

--- typescript/Aula 22-06/criapessoa.ts
+++ typescript/Aula 22-06/criapessoa.ts
@@ -1,6 +1,9 @@
 import { Pessoa } from "./pessoa";
 
-var pessoa1 = new Pessoa (1 , "ruan" , 33873467, "mail")
-console.log(pessoa1.validar())
-console.log(pessoa1.imprimirnatela())
-console.log(pessoa1.salvar())
+var ruan = new Pessoa (1 , "ruan" , 33873467, "ruan@mail.com" , 58, 1.78 )
+
+var ana = new Pessoa(2, "ana" , 123456 , "ana@mail.com", 65, 1.59)
+
+var joao = new Pessoa(3, "joão" , 12345689 , "joao@mail.com" , 90 , 1.95)
+
+console.log(ruan.imc())
--- typescript/Aula 22-06/pessoa.ts
+++ typescript/Aula 22-06/pessoa.ts
@@ -3,13 +3,17 @@ class Pessoa{
     nome: string
     fone: number
     email: string
+    peso: number
+    altura: number
 
 
-    constructor(id: number, nome: string, fone: number, email: string){
+    constructor(id: number, nome: string, fone: number, email: string, peso: number, altura: number){
         this.id = id
         this.nome = nome
         this.fone = fone
         this.email = email
+        this.peso = peso
+        this.altura = altura
     }
 
     salvar(): void{
@@ -34,6 +38,13 @@ class Pessoa{
         console.log(`nome: ${this.nome}`)
         console.log(`fone: ${this.fone}`)
         console.log(`email: ${this.email}`)
+    }
+
+    imc():number{
+        var altura2: number
+        altura2 = parseFloat((this.altura * this.altura).toFixed(2))
+
+        return (this.peso / altura2)
     }
 }
 
