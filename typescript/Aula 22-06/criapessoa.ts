9c83009bbb10b82fd639efcead898648278130dd
Ruan Karl Alves de Souza <ruansouza5@gmail.com>
2022-06-22 21:08:57 -0300

    adicionado array com dados da classe

--- typescript/Aula 22-06/criapessoa.ts
+++ typescript/Aula 22-06/criapessoa.ts
@@ -7,3 +7,13 @@ var ana = new Pessoa(2, "ana" , 123456 , "ana@mail.com", 65, 1.59)
 var joao = new Pessoa(3, "joão" , 12345689 , "joao@mail.com" , 90 , 1.95)
 
 console.log(ruan.imc())
+
+
+let pessoas: Pessoa[] = []
+pessoas.push(ruan)
+pessoas.push(ana)
+pessoas.push(joao)
+pessoas.push(new Pessoa(4, "maria", 33874521, "maria@mail.com", 52 , 1.60))
+
+/*Percorre o array pessoas que contem os dados de Pessoa*/
+pessoas.forEach((p) => p.imprimirnatela())
